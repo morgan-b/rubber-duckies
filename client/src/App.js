a91fece657ebf3536ac2b2fa8bf0e58c80747a44
nqg <nidaghuman@gmail.com>
2021-05-19 20:11:17 -0400

    file structire changes, fixed package.json issue

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,8 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello guys! We got it to work! CELEBRATE!
     </div>
   );
 }
