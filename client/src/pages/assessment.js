c0a7e5ccb22aed059ec3bdbfc3f9ad7e6d998ebb
Morgan Bailey <mabailey079@Gmail.com>
2021-05-22 11:09:37 -0400

    adding search functionality

--- client/src/pages/assessment.js
+++ client/src/pages/assessment.js
@@ -8,68 +8,64 @@ import BlankImage from "../assets/blank-square.jpg";
 function Assessment() {
   const [formObject, setFormObject] = useState({});
   const [resCards, setResCards] = useState([]);
+  const [search, setSearch] = useState([]);
 
   function handleInputChange(event) {
     const { name, value } = event.target;
-    setFormObject({ ...formObject, [name]: value });
-    console.log(formObject)
+    setFormObject({ ...formObject, value });
+    console.log(formObject);
   }
 
-  function onClick() {
-
-
-        
-      }
-
+  function onClick() {}
 
   function handleFormSubmit(event) {
+
+
     event.preventDefault();
-    SearchApi.GetImages()
+    SearchApi.GetImages(formObject.value)
 
-    .then((response) => setResCards(response.data.value))
-    .then(() => console.log(resCards))
-    .catch((err) => console.log(err));
-
+      .then((response) => setResCards(response.data.value))
+      .then(() => console.log(resCards))
+      .catch((err) => console.log(err));
   }
   return (
     <div className="Container">
       <NavBar />
-      <AssessWrapper onClick={handleFormSubmit}
-      onChange={handleInputChange}>
-
-{resCards.length ? (
+      <AssessWrapper onClick={handleFormSubmit} onChange={handleInputChange}>
+        {resCards.length ? (
           resCards.map((resCard) => (
-        <div className="col">
-          <AssessCard key={resCard.thumbnail} id={resCard.thumbnail} thumbnail={resCard.thumbnail}></AssessCard>
-        </div>
+            <div className="col imgcol">
+              <AssessCard
+                key={resCard.thumbnail}
+                id={resCard.thumbnail}
+                thumbnail={resCard.thumbnail}
+              ></AssessCard>
+            </div>
           ))
-          ) : (
-              <div>
-            <div className="col">
-            <AssessCard thumbail={BlankImage}></AssessCard>
-          </div>
-          <div className="col">
-          <AssessCard ></AssessCard>
-        </div>
-        <div className="col">
-        <AssessCard ></AssessCard>
-      </div>
-      <div className="col">
-      <AssessCard ></AssessCard>
+        ) : (
+          <>
+            <div className="col imgcol">
+              <AssessCard key={1} thumbnail={BlankImage}></AssessCard>
+            </div>
+            <div className="col imgcol">
+              <AssessCard key={2} thumbnail={BlankImage}></AssessCard>
+            </div>
+            <div className="col imgcol">
+              <AssessCard key={3} thumbnail={BlankImage}></AssessCard>
+            </div>
+            <div className="col imgcol">
+              <AssessCard key={4} thumbnail={BlankImage}></AssessCard>
+            </div>
+            <div className="col imgcol">
+              <AssessCard key={5} thumbnail={BlankImage}></AssessCard>
+            </div>
+            <div className="col imgcol">
+              <AssessCard key={6} thumbnail={BlankImage}></AssessCard>
+            </div>
+          </>
+        )}
+      </AssessWrapper>
     </div>
-    <div className="col">
-    <AssessCard ></AssessCard>
-  </div>
-  <div className="col">
-  <AssessCard ></AssessCard>
-</div>
-</div>
- )}
- </AssessWrapper>
- </div>
-    
-
-   
   );
 }
 export default Assessment;
