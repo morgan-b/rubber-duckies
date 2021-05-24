8960885728c2023574467001ea19a51ddddf5049
Bhavika Patel <bhavij28@gmail.com>
2021-05-24 18:51:09 -0400

    schemasql2

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,5 @@
-DROP DATABASE IF EXISTS rubber_duckiesDB;
-CREATE database rubber_duckiesDB;
-
 USE rubber_duckiesDB;
 
----USERNEEDS---
 CREATE TABLE USERNEEDS (
 UserNeedsId INT NOT NULL AUTO_INCREMENT,
 UserNeedsDescription VARCHAR(50),
@@ -12,8 +8,6 @@ PRIMARY KEY (UserNeedsId)
 INSERT INTO USERNEEDS (UserNeedsDescription)
 VALUES ("Hungry"),("Thirsty"),("Restroom") ;
 
-
----USEREMOTIONS---
 CREATE TABLE USEREMOTIONS (
 UserEmotionId INT NOT NULL AUTO_INCREMENT,
 EmotionDescription VARCHAR(50),
@@ -23,8 +17,6 @@ INSERT INTO USEREMOTIONS (EmotionDescription)
 VALUES ("Happy"),("Sad"),("Nervous") ;
 
 
-
----CAREGIVER----
 CREATE TABLE CAREGIVER (
 caregiverid INT NOT NULL AUTO_INCREMENT,
 username VARCHAR(50) NULL,
@@ -36,7 +28,6 @@ PRIMARY KEY(caregiverid)
 
 );
 
---USERS---
 CREATE TABLE USERS  (
 
 Userid INT NOT NULL AUTO_INCREMENT,
@@ -50,7 +41,7 @@ PRIMARY KEY (userid),
 FOREIGN KEY (caregiverid) REFERENCES CAREGIVER (caregiverid) 
 );
 
----USERACTIONDETAIL---
+
 CREATE TABLE USERACTIONDETAIL (
 
 UserActionDetailId INT NOT NULL AUTO_INCREMENT,
@@ -66,10 +57,3 @@ FOREIGN KEY (userid) REFERENCES USERS (userid)
 
 );
 
-
-select * from USERSTAG;
-select * from USEREMOTIONS;
-select * from CAREGIVER;
-select * from USERNEEDS;
-SELECT * FROM USERS;
-select * from USERACTIONDETAIL;
