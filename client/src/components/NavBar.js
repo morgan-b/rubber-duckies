868caaf74e18e8439902957cc1e2d884165fb1f2
nqg <nidaghuman@gmail.com>
2021-06-02 00:42:06 -0400

    added piecharts with react-vis npm, styled user details table, set navbar to display routes depending on user type

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,45 +1,69 @@
-import session from 'express-session';
-import React from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import session from "express-session";
+import React, {useState, useEffect} from "react";
+import { Link, useHistory } from "react-router-dom";
 import logo from "../assets/logo-duckies.png";
 import Apiroutes from "../utils/Apiroutes";
 
-
-
 function NavBar() {
+  const [userloggedin, setUserloggedin] = useState(false);
 
-const history = useHistory()
+  useEffect(() => {
+    const auth = localStorage.getItem("user");
+    console.log(auth);
 
-const handleLogout = () => {
-Apiroutes.logOut()
-.then (res => {
-    console.log("logged out")
-    localStorage.clear()
-    history.push("/login")
-}
-    )
-}
+    setUserloggedin(auth);
+  }, []);
 
+  const history = useHistory();
 
-    return (
+  const handleLogout = () => {
+    Apiroutes.logOut().then((res) => {
+      console.log("logged out");
+      localStorage.clear();
+      history.push("/login");
+    });
+  };
 
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src={logo} alt="logo" width="45"/></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavLinks" aria-controls="navbarNavLinks" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-              </button>
-              <div className="collapse navbar-collapse justify-content-end" id="navbarNavLinks">
-                        <div className="navbar-nav">  
-                        <Link className="nav-link" to="/profile">Profile</Link>
-                        <Link className="nav-link" to="/home">Dashboard</Link>
-                        <div className="nav-link logoutBtn" onClick={handleLogout}>Logout</div>
-                    </div>
-              </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to={userloggedin ? ("/home"):("/profile")}>
+          <img src={logo} alt="logo" width="45" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavLinks"
+          aria-controls="navbarNavLinks"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavLinks"
+        >
+          <div className="navbar-nav">
+            {userloggedin ? (
+              <Link className="nav-link" to="/home">
+                Dashboard
+              </Link>
+            ) : (
+              <Link className="nav-link" to="/profile">
+                Profile
+              </Link>
+            )}
+
+            <div className="nav-link logoutBtn" onClick={handleLogout}>
+              Logout
             </div>
-        </nav>
-
-    )
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
--- client/src/pages/userDetails.js
+++ client/src/pages/userDetails.js
@@ -1,118 +1,188 @@
-import React, {useState, useEffect, Component} from 'react';
-import {useLocation} from 'react-router-dom';
-import NavBar from "../components/NavBar"
-import logo from "../assets/logo-duckies.png"
-import Apiroutes from '../utils/Apiroutes';
-import BarChart from "../components/UserChart"
-
-
-
+import React, { useState, useEffect, Component } from "react";
+import { useLocation } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import logo from "../assets/logo-duckies.png";
+import Apiroutes from "../utils/Apiroutes";
+import BarChart from "../components/UserChart";
+//import RadialChart from "../components/RadialChart";
+import { RadialChart } from "react-vis";
 
 function userDetails() {
-const [userData, setUserData] = useState([]);
-const [actions, setActions] = useState([]);
-const [timeStamp, setTimeStamps] = useState([]);
+  const [userData, setUserData] = useState([]);
+  const [actions, setActions] = useState([]);
+  const [timeStamp, setTimeStamps] = useState([]);
 
+  //uselocation hook to get data from one page to the next.
+  //used this to pass on user data depending on which user is clicked on
+  const { state } = useLocation();
 
+  console.log(state);
 
+  useEffect(() => {
+    setUserData(state);
+    let userid = state.userid;
+    console.log(userid);
 
-//uselocation hook to get data from one page to the next. 
-//used this to pass on user data depending on which user is clicked on
-const {state} = useLocation()
+    populateUserDetails(userid);
+  }, []);
 
-console.log(state)
+  const populateUserDetails = (userid) => {
+    Apiroutes.userDetails(userid)
 
-useEffect(() => {
-  setUserData(state)
- let userid = state.userid
- console.log(userid)
+      .then((res) => {
+        console.log("user found!");
 
-  populateUserDetails(userid)
-  
+        setActions(res.data);
+        setTimeStamps(res.data);
 
-}, [])
+        console.log(res.data);
+      })
 
+      .catch((err) => console.log(err));
+  };
 
-const populateUserDetails = (userid) => {
+  //function to populate piechart using react-vis npm
+  const pieChart = () => {
+    let emData = {
+      happy: 0,
+      sad: 0,
+      nervous: 0,
+    };
 
-
-
-  Apiroutes.userDetails(userid)
-
-
-  .then (res => {
-   console.log("user found!")
-
-    setActions(res.data)
-    setTimeStamps(res.data)
-
-    
-    console.log(res.data)
-   
-  })
- 
-  
-    
-.catch(err => console.log(err));
-}
-
-
-
-    return (
-        <>
-        <NavBar/>
-        <div className="px-4 py-5 my-5 text-center">
-    <img className="d-block mx-auto mb-4" src={logo} alt="logo" style={{'width':'100px'}}/>
-    <h1 className="display-5 fw-bold">{userData.firstname} {userData.lastname}</h1>
-    <div className="col-lg-6 mx-auto">
-      <p className="lead mb-4">View reports below</p>
-    </div>
-  </div>  
-
-
-
-
-<BarChart  data={timeStamp}>
-
-</BarChart>
-
-
-
-      <div className="container">
-
-      <table className="table table-striped">
-  <thead>
-    <tr key="8">
-      <th key="6"scope="col">Needs</th>
-      <th key="7"scope="col">Date/Time</th>
-    </tr>
-  </thead>
-
-  <tbody>
-  {actions.length ? (
-    actions.map((action) =>(
-
-
-   
-    <tr key={action.userActionDetailId+3}>
-      <td key={action.userActionDetailId+1}>  {action.userInput}</td>
-      <td key={action.userActionDetailId+2}>{new Date(action.date_created).toString()}</td>
-    </tr>
-       ))) :(
-        <tr>
-        <th key="1" scope="row">1</th>
-        <td key="2"> awaiting new action</td>
-        <td key="3" > awaiting new action</td>
-      </tr>
-       )
+    actions.forEach((action) => {
+      if (action.userInput === "Happy") {
+        emData.happy += 1;
+      } else if (action.userInput === "Sad") {
+        emData.sad += 1;
+      } else if (action.userInput == "Nervous") {
+        emData.nervous += 1;
       }
-  </tbody>
-</table>        
+    });
+    console.log(emData);
 
+    return [
+      { angle: emData.happy, label: "Happy" },
+      { angle: emData.sad, label: "Sad" },
+      { angle: emData.nervous, label: "Nervous" },
+    ];
+  };
 
-        </div>    
-        </>
-    )
+//function to populate piechart using react-vis npm
+const pieChart2 = () => {
+  let needsData = {
+    restroom: 0,
+    thirsty: 0,
+    hungry: 0,
+  };
+
+  actions.forEach((action) => {
+    if (action.userInput === "Restroom") {
+      needsData.restroom += 1;
+    } else if (action.userInput === "Thirsty") {
+      needsData.thirsty += 1;
+    } else if (action.userInput == "Hungry") {
+      needsData.hungry += 1;
+    }
+  });
+  console.log(needsData);
+
+  return [
+    { angle: needsData.restroom, label: "Restroom" },
+    { angle: needsData.thirsty, label: "Thirsty" },
+    { angle: needsData.hungry, label: "Hungry" },
+  ];
+};
+
+  return (
+    <>
+      <NavBar />
+      <div className="px-4 py-5 my-5 text-center">
+        <img
+          className="d-block mx-auto mb-4"
+          src={logo}
+          alt="logo"
+          style={{ width: "100px" }}
+        />
+        <h1 className="display-5 fw-bold">
+          {userData.firstname} {userData.lastname}
+        </h1>
+        <div className="col-lg-6 mx-auto">
+          <p className="lead mb-4">View reports below</p>
+        </div>
+      </div>
+
+      <div className="container text-center justify-content-center">
+        <table className="table table-striped">
+          <thead>
+            <tr key="8">
+              <th key="6" scope="col">
+                Needs
+              </th>
+              <th key="7" scope="col">
+                Date
+              </th>
+              <th key="8" scope="col">
+                Time Stamp
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {actions.length ? (
+              actions.map((action) => (
+                <tr key={action.userActionDetailId + 3}>
+                  <td key={action.userActionDetailId + 1}>
+                    {" "}
+                    {action.userInput}
+                  </td>
+                  <td key={action.userActionDetailId + 2}>
+                    {new Date(action.date_created).toString().substring(0, 15)}
+                  </td>
+                  <td key={action.userActionDetailId + 2}>
+                    {new Date(action.date_created).toString().substring(15, 57)}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <th key="1" scope="row">
+                  1
+                </th>
+                <td key="2"> awaiting new action</td>
+                <td key="3"> awaiting new action</td>
+                <td key="4"> awaiting new action</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+
+        <div className="row">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-2">
+            <div className="col">
+              <RadialChart
+                data={pieChart()}
+                width={300}
+                height={300}
+                showLabels={true}
+                labelsAboveChildren={true}
+              />
+            </div>
+            <div className="col">
+              <RadialChart
+                data={pieChart2()}
+                width={300}
+                height={300}
+                showLabels={true}
+                labelsAboveChildren={true}
+              />
+            </div>
+          </div>
+        </div>
+
+        <BarChart data={timeStamp}></BarChart>
+      </div>
+    </>
+  );
 }
 
-export default userDetails
+export default userDetails;
