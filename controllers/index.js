3994ff99735fc5da0692b6122d1c7535a9457e1c
nqg <nidaghuman@gmail.com>
2021-06-19 16:45:09 -0400

    removed 404 status from routes

--- controllers/index.js
+++ controllers/index.js
@@ -6,9 +6,9 @@ const userRoutes = require("./userRoutes");
 router.use("/api", apiRoutes);
 router.use("/", userRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
+//router.use((req, res) => {
+ // res.status(404).end();
+//});
 
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
